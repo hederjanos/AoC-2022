46ad244ddbac303940ea55c2d137ff8b27778725
hederjanos <hederjanos@gmail.com>
2022-12-03 16:30:04 +0100

    solve part two of day3

--- src/main/java/day/_3/RucksackReOrganizer.java
+++ src/main/java/day/_3/RucksackReOrganizer.java
@@ -3,7 +3,9 @@ package day._3;
 import util.common.Solver;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class RucksackReOrganizer extends Solver<Integer> {
@@ -15,11 +17,11 @@ public class RucksackReOrganizer extends Solver<Integer> {
     @Override
     protected Integer solvePartOne() {
         return puzzle.stream()
-                .map(this::convertToPriority)
+                .map(this::convertRucksackItemsToPriority)
                 .reduce(0, Integer::sum);
     }
 
-    private int convertToPriority(String items) {
+    private int convertRucksackItemsToPriority(String items) {
         Set<Character> firstCompartment = new HashSet<>();
         Set<Character> secondCompartment = new HashSet<>();
         int halfOfItems = items.length() / 2;
@@ -30,13 +32,31 @@ public class RucksackReOrganizer extends Solver<Integer> {
                     secondCompartment.add(items.charAt(i + halfOfItems));
                 });
         firstCompartment.retainAll(secondCompartment);
-        char intersection = firstCompartment.stream().findFirst().orElseThrow();
+        return calculatePriority(firstCompartment.stream().findFirst().orElseThrow());
+    }
+
+    private int calculatePriority(char intersection) {
         return Character.isUpperCase(intersection) ? intersection - 'A' + 27 : intersection - 'a' + 1;
     }
 
     @Override
     protected Integer solvePartTwo() {
-        return null;
+        return IntStream.iterate(0, i -> i < puzzle.size(), i -> i + 3)
+                .mapToObj(i -> puzzle.subList(i, i + 3))
+                .map(this::convertGroupItemsToPriority)
+                .reduce(0, Integer::sum);
+    }
+
+    public int convertGroupItemsToPriority(List<String> items) {
+        List<Set<Character>> groups = items.stream()
+                .map(item -> IntStream.range(0, item.length())
+                        .boxed()
+                        .map(item::charAt)
+                        .collect(Collectors.toSet()))
+                .collect(Collectors.toList());
+        HashSet<Character> commonItems = groups.stream()
+                .collect(() -> new HashSet<>(groups.get(0)), Set::retainAll, Set::retainAll);
+        return calculatePriority(commonItems.stream().findFirst().orElseThrow());
     }
 
 }
